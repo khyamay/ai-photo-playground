87aa2ec9deb278071428cfda5ed617d14a3e6492
Khemraj Thapa <khyamay@gmail.com>
2024-07-30 20:57:30 +0100

    feat: theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     join(__dirname, 'libs/**/!(*.stories|*.spec).{ts,html}'),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          lighter: '#fcd34d',
+          light: '#fbbf24',
+          main: '#f59e0b',
+          dark: '#d97706',
+          darker: '#b45309',
+        },
+        secondary: {
+          lighter: '#c4b5fd',
+          light: '#a78bfa',
+          main: '#8b5cf6',
+          dark: '#7c3aed',
+          darker: '#6d28d9',
+        },
+      },
+    },
   },
   plugins: [],
 };
